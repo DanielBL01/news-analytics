66c039173cadd99614e12d13d0718b6dfba7a681
DanielBL01 <dblee0532@gmail.com>
2021-04-19 22:47:17 -0400

    add redirects and use chartjs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,13 +6,17 @@ app.secret_key = 'YOUR SECRET KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'YOUR DATABASE URI'
 db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return redirect(url_for('results', url = request.form['url']))
+
     return render_template('index.html')
 
 @app.route('/results')
 def results():
-    return render_template('results.html')
+    url = request.args['url']
+    return render_template('results.html', url = url)
 
 if __name__ == '__main__':
     app.run(debug=True)
