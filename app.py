c14082a0466a653147c928baae2ea0fd6634acc7
DanielBL01 <dblee0532@gmail.com>
2021-05-12 02:30:23 -0400

    save to postgres

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from analytics.analysis import getAnalysisResults
 from analytics.sentiment import sentimentAnalysis
 from analytics.frequency import getWordFrequency
+from db.models import *
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,6 +22,11 @@ def index():
 def results():
     try:
         url = request.args['url']
+
+        record = Result.query.filter_by(url = url).first()
+        if record:
+            return render_template('results.html', summary = record.summary, sentiment = record.sentiment, display = record.licommon, most_common = record.mcommon)
+
         text, summary = getAnalysisResults(url)
         sentiment = sentimentAnalysis(text)
         words, most_common = getWordFrequency(text)
@@ -31,7 +37,13 @@ def results():
                 display = display + word + ', '
             else:
                 display = display + word
+        
+        result = Result(url = url, sentiment = sentiment, summary = summary, mcommon = most_common, licommon = display)
+        db.session.add(result)
+        db.session.commit()
+
         return render_template('results.html', summary = summary, sentiment = sentiment, display = display, most_common = most_common)
+
     except:
         return render_template('error.html')
         
--- db/models.py
+++ db/models.py
@@ -2,6 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Analytics(db.Model):
-    """ Analytics Model """
-    pass
+class Result(db.Model):
+    __tablename__ = 'results'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    url = db.Column(db.String(), unique = True, nullable = False)
+    sentiment = db.Column(db.String(15), nullable = False)
+    summary = db.Column(db.String(), unique = True, nullable = False)
+    mcommon = db.Column(db.String(45))
+    licommon = db.Column(db.String())
